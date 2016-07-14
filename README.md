d2bf2db4e00359f35addcee133c799e9d42a9abd
Nathan Friedly <nathan@nfriedly.com>
2016-07-14 17:44:42 -0400

    initial commit, basic functionality works

---
+++ README.md
@@ -0,0 +1,12 @@
+# Contentful Dictate
+
+A UI Extension for [Contentful] that uses [IBM Watson Speech to Text] to enable voice dictation.
+
+Designed to be hosted on [Bluemix].
+
+You'll need to update the `extension.json` file with your server's url, and then follow [the guide] to create your own extension.
+
+[Contentful]: https://www.contentful.com/
+[IBM Watson Speech to Text]: http://www.ibm.com/watson/developercloud/speech-to-text.html
+[Bluemix]: https://console.ng.bluemix.net/
+[the guide]: https://www.contentful.com/r/knowledgebase/ui-extensions-guide/
---
+++ app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+var expressBrowserify = require('express-browserify');
+var vcapServices = require('vcap_services');
+var extend = require('util')._extend;
+var watson = require('watson-developer-cloud');
+
+require('dotenv').load({silent: true}); // loads environment properties from a .env file for local development
+
+app.set('view engine', 'ejs');
+
+// automatically bundle the front-end js on the fly
+// note: this should come before the express.static since bundle.js is in the public folder
+app.get('/js/bundle.js', expressBrowserify('./public/js/bundle.js', {
+    watch: (app.get('env') === 'development')
+}));
+
+// Setup static public directory
+app.use(express.static('./public'));
+
+
+// Watson Speech to Text access token generator
+// For local development, replace username and password or set env properties
+// When running on bluemix, vcapServices will override these with bluemix-provided credentials
+var sttConfig = extend({
+    version: 'v1',
+    url: 'https://stream.watsonplatform.net/speech-to-text/api',
+    username: process.env.SPEECH_TO_TEXT_USERNAME || '<username>',
+    password: process.env.SPEECH_TO_TEXT_PASSWORD || '<password>'
+}, vcapServices.getCredentials('speech_to_text'));
+var sttAuthService = watson.authorization(sttConfig);
+
+app.get('/', function(req, res) {
+    sttAuthService.getToken({url: sttConfig.url}, function(err, token) {
+        if (err) {
+            console.log('Error retrieving token: ', err);
+            res.status(500).send('Error retrieving token');
+            return;
+        }
+        res.render('index', {token: token});
+    });
+});
+
+
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('server listening on http://localhost:%s/', port);
